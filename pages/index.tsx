e3740988cceeaa5f322eb2eec692a3d90060511b
Thierry <tianming.chen0424@gmail.com>
2023-04-24 23:53:33 -0400

    refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ import {
   SiNextdotjs,
   SiReact,
   SiTailwindcss,
+  SiVercel,
 } from "react-icons/si";
 import Image from "next/image";
 import deved from "../public/dev-ed-wave.png";
@@ -25,6 +26,9 @@ import job2 from "../public/job2.png";
 import job3 from "../public/job3.png";
 import job4 from "../public/job4.png";
 import { useState } from "react";
+import web1 from "../public/web1.png";
+import web2 from "../public/web2.png";
+import { title } from "process";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -34,6 +38,11 @@ export default function Home() {
   const urlInstagram = "https://www.instagram.com/dream_ins_spiration/";
   const urlResume =
     "https://drive.google.com/file/d/1ws_zF5NWkiuNjjOoplQPygGm3AyGORsH/view?usp=sharing";
+
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: "smooth" });
+  };
 
   return (
     <div className={darkMode ? "dark" : ""}>
@@ -47,31 +56,67 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <section
-        className="p-0 lg:px-8 lg:py-4 bg-gray-100 flex items-center justify-between  text-gray-900"
-        aria-label="Global"
-      >
-        <div className="hidden lg:flex gap-x-12">
-          <a href="#" className="text-sm font-semibold leading-6">
+      <div className="p-0 lg:px-8 lg:py-4 bg-gray-100 flex items-center justify-between  text-gray-900">
+        <div className="hidden lg:flex gap-x-12 text-gray-400 ">
+          <a
+            href=""
+            className="text-sm font-semibold leading-6 cursor-not-allowed"
+          >
             Entertainement
           </a>
-          <a href="#" className="text-sm font-semibold leading-6">
-            Housing
+          <a
+            href=""
+            className="text-sm font-semibold leading-6 cursor-not-allowed"
+          >
+            Investing
           </a>
-          <a href="#" className="text-sm font-semibold leading-6">
+          <a
+            href=""
+            className="text-sm font-semibold leading-6 cursor-not-allowed"
+          >
             Media
           </a>
+          <a
+            href=""
+            className="text-sm font-semibold leading-6 cursor-not-allowed"
+          >
+            Blog
+          </a>
+          <a
+            href=""
+            className="text-sm font-semibold leading-6 cursor-not-allowed"
+          >
+            Store
+          </a>
+          <a
+            href=""
+            className="text-sm font-semibold leading-6 cursor-not-allowed"
+          >
+            Design
+          </a>
         </div>
-      </section>
+      </div>
 
       <main className="bg-white dark:bg-gray-900 dark:text-white">
         {/* CONTENT WRAPPER */}
         <div className="px-2 md:px-20 lg:px-40 max-w-screen-2xl mx-auto">
-          <section>
+          <section className="">
             {/* NAVBAR */}
             <nav className="px-5 py-5 mb-0 sm:py-10 md:mb-12 flex justify-between">
               {/* font-burtons was defined in tailwind config */}
-              <h1 className=" text-xl font-burtons">Thierry</h1>
+              <h1
+                className="text-2xl pt-1 font-burtons"
+                onClick={() => window.location.reload()}
+              >
+                Thierry
+              </h1>
+
+              <div
+                onClick={() => scrollToSection("projects")}
+                className="bg-transparent invisible hidden md:block  md:visible text-slate-700 dark:hover:border-transparent dark:text-white hover:bg-gradient-to-br hover:from-[#ff8a05] hover:via-[#ff5478] hover:to-[#ff00c6]  border-slate-400 hover:border-white hover:text-white py-2 px-4 border  rounded  text-sm  font-semibold  shadow-md shadow-slate-300 hover:shadow-none dark:shadow-none hover:scale-105 transition duration-200"
+              >
+                Build your website
+              </div>
               <ul className="flex items-center">
                 <li>
                   {/* button dark mode toogle */}
@@ -92,6 +137,14 @@ export default function Home() {
                 </li>
               </ul>
             </nav>
+            <nav className="px-5 mb-0">
+              <div
+                onClick={() => scrollToSection("projects")}
+                className="bg-transparent visible md:invisible md:block text-center text-slate-700 dark:hover:border-transparent dark:text-white hover:bg-gradient-to-br hover:from-[#ff8a05] hover:via-[#ff5478] hover:to-[#ff00c6]  border-slate-400 hover:border-white hover:text-white py-2 px-4 border  rounded  text-sm  font-semibold  shadow-md shadow-slate-300 hover:shadow-none dark:shadow-none hover:scale-105 transition duration-200"
+              >
+                Build your website
+              </div>
+            </nav>
 
             {/* CONTENT */}
             <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap text-center">
@@ -101,7 +154,8 @@ export default function Home() {
                   <span className="">Thierry Chen</span>
                 </h2>
                 <h3 className="text-2xl py-2 md:text-3xl">
-                  Software Developer. Montreal
+                  Software Developer & Designer
+                  <div className="text-xl pt-2">Montreal</div>
                 </h3>
                 {/* max-w-wl for max width paragraph */}
                 <p className="text-md py-5 leading-8 text-gray-800 dark:text-white md:text-xl max-w-xl mx-auto hover:scale-105 transition duration-300">
@@ -234,9 +288,9 @@ export default function Home() {
             </h3>
 
             {/* JOB */}
-            <div className="lg:flex gap-5">
+            <div className="lg:flex gap-5 ">
               {/* ZIMMER */}
-              <div className="bg-gradient-to-r from-blue-700 to-blue-500 shadow-lg shadow-blue-500/50 text-center text-white font-semibold py-10 px-4 rounded-xl my-10 flex-1 hover:brightness-105 hover:scale-105 transition duration-300">
+              <div className="bg-gradient-to-r from-blue-700 to-blue-600 shadow-lg shadow-blue-500/50 text-center text-white font-semibold py-10 px-4 rounded-xl my-10 flex-1 hover:brightness-105 hover:scale-105 transition duration-300">
                 <Image
                   className="mx-auto"
                   src={job4}
@@ -262,7 +316,7 @@ export default function Home() {
                 <p className="text-gray-100">Mockito</p>
               </div>
               {/* ERICSSON */}
-              <div className="bg-sky-400 shadow-md text-center text-white font-semibold py-10 px-4 rounded-xl my-10  flex-1 hover:brightness-105 hover:scale-105 transition duration-300">
+              <div className="bg-blue-500 shadow-md text-center text-white font-semibold py-10 px-4 rounded-xl my-10  flex-1 hover:brightness-105 hover:scale-105 transition duration-300">
                 <Image
                   className="mx-auto"
                   src={job3}
@@ -283,7 +337,7 @@ export default function Home() {
                 <p className="text-gray-100">Ubuntu</p>
               </div>
               {/* FIERA */}
-              <div className="bg-sky-400 shadow-md text-center text-white font-semibold py-10 px-4 rounded-xl my-10  flex-1   hover:brightness-105 hover:scale-105 transition duration-300">
+              <div className="bg-blue-500 shadow-md text-center text-white font-semibold py-10 px-4 rounded-xl my-10  flex-1   hover:brightness-105 hover:scale-105 transition duration-300">
                 <Image
                   className="mx-auto"
                   src={job2}
@@ -304,7 +358,7 @@ export default function Home() {
                 <p className="text-gray-100">SQL</p>
               </div>
               {/* AIRBUS */}
-              <div className="bg-sky-400 shadow-md text-center text-white font-semibold py-10 px-4 rounded-xl my-10 flex-1  hover:brightness-105 hover:scale-105 transition duration-300">
+              <div className="bg-blue-500 shadow-md text-center text-white font-semibold py-10 px-4 rounded-xl my-10 flex-1  hover:brightness-105 hover:scale-105 transition duration-300">
                 <Image
                   className="mx-auto pt-3"
                   src={job1}
@@ -326,74 +380,85 @@ export default function Home() {
             </div>
           </section>
 
-          {/* DO RIGHT NOW */}
-          <section className="text-gray-100 bg-gradient-to-r from-blue-900 from-15% to-blue-950 text-white overflow-hidden shadow-md shadow-blue-950/50 rounded-xl flex-1 my-5 grid grid-cols-1 md:grid-cols-3 hover:brightness-110 transition duration-300">
-            <div className="col-span-2 m-10">
-              <h1 className="text-2xl font-bold text-center flex items-center mx-auto justify-center">
-                <BsListTask className="pt-1" />
-                &nbsp;What I am doing right now
-              </h1>
-              <div className="text-gray-300 flex items-center mx-auto justify-center">
-                <ul className="text-xl pl-1 sm:pl-10 pt-3 space-y-3 list-disc marker:text-blue-600">
-                  <li>Learning Azure</li>
-                  <li>
-                    <div className="flex items-center">
-                      Playing AI drawing tools: Stable Diffusion & Midjourney &
-                      etc&nbsp;&nbsp;
-                      <span className="group relative">
-                        <AiFillInstagram
-                          className="text-3xl cursor-pointer  hover:scale-125 transition duration-100 text-pink-400"
-                          onClick={() => window.open(urlInstagram, "_blank")}
-                        />
-                      </span>
-                    </div>
-                  </li>
-                  <li>Develop a mobile game using unity</li>
-                  <li>Plannig for personal vlog</li>
-                </ul>
+          {/* PLANS */}
+          <section className="section-plan h-screen">
+            <section className="text-gray-100 bg-gradient-to-r from-blue-900 from-15% to-blue-950 text-white overflow-hidden shadow-md shadow-blue-950/50 rounded-xl flex-1 my-5 grid grid-cols-1 md:grid-cols-3 hover:brightness-110 transition duration-300">
+              <div className="col-span-2 m-10">
+                <h1 className="text-2xl font-bold text-center flex items-center mx-auto justify-center">
+                  <BsListTask className="pt-1" />
+                  &nbsp;What I am doing right now
+                </h1>
+                <div className="text-gray-300 flex items-center mx-auto justify-center font-semibold">
+                  <ul className="text-xl pl-1 sm:pl-10 pt-3 space-y-3 list-disc marker:text-blue-600">
+                    <li>Learning Azure</li>
+                    <li>
+                      <div className="flex items-center">
+                        Playing AI drawing tools: Stable Diffusion & Midjourney
+                        & etc&nbsp;&nbsp;
+                        <span className="group relative">
+                          <AiFillInstagram
+                            className="text-3xl cursor-pointer  hover:scale-125 transition duration-100 text-pink-400"
+                            onClick={() => window.open(urlInstagram, "_blank")}
+                          />
+                        </span>
+                      </div>
+                    </li>
+                    <li>Develop a mobile game using unity</li>
+                    <li>Plannig for personal vlog</li>
+                  </ul>
+                </div>
               </div>
-            </div>
-            <div className="img-type-1 col-span-1 bg-black rounded-r-xl border-l-4 dark:border-black hover:border-l-2"></div>
-          </section>
+              <div className="img-type-1 col-span-1 bg-black rounded-r-xl border-l-4 dark:border-black hover:border-l-2"></div>
+            </section>
 
-          {/* DO LATER */}
-          <section className="mt-10 text-gray-100 bg-gradient-to-r from-blue-900 from-10% to-blue-950 text-white py-10 shadow-md shadow-blue-950/50 p-10 rounded-xl hover:brightness-110 transition duration-300">
-            <h1 className="text-3xl py-2 dark:text-white text-center flex items-center mx-auto justify-center">
-              Next Plan
-            </h1>
-            <div className="pb-2 text-center text-xl  text-gray-300 list-disc dark:text-white">
-              <div className="flex justify-center flex items-center">
-                <FaRobot />
-                &nbsp;AI and ML&nbsp;
-                <FaRobot />
+            {/* DO LATER */}
+            <section className="mt-10 text-gray-100 bg-gradient-to-r from-blue-900 from-10% to-blue-950 text-white py-10 shadow-md shadow-blue-950/50 p-10 rounded-xl hover:brightness-110 transition duration-300">
+              <h1 className="text-3xl py-2 dark:text-white text-center flex items-center mx-auto justify-center">
+                Next Plan
+              </h1>
+              <div className="pb-2 text-center text-xl  text-gray-300 list-disc dark:text-white">
+                <div className="flex justify-center flex items-center">
+                  <FaRobot />
+                  &nbsp;AI and ML&nbsp;
+                  <FaRobot />
+                </div>
               </div>
-            </div>
+            </section>
           </section>
 
           {/*  */}
-          <section>
+          <section id="projects" className="mt-11">
+            <h3 className="text-5xl pb-2 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-cyan-500 font-extrabold w-fit">
+              Projects
+            </h3>
             <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
               <div className="basis-1/3 flex-1">
-                {/* <Image
+                <Image
                   src={web1}
                   className="rounded-lg object-cover"
                   width={"100%"}
                   height={"100%"}
                   layout="responsive"
                   alt=""
-                /> */}
+                />
               </div>
               <div className="basis-1/3 flex-1">
-                {/* <Image
+                <Image
                   src={web2}
                   className="rounded-lg object-cover"
                   width={"100%"}
                   height={"100%"}
                   layout="responsive"
                   alt=""
-                /> */}
+                />
               </div>
             </div>
+          </section>
+
+          <section className="py-12 mt-12">
+            <h3 className="text-5xl py-1 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-cyan-500 font-extrabold w-fit">
+              Project
+            </h3>
           </section>
 
           {/*  */}
@@ -402,15 +467,27 @@ export default function Home() {
               This page is powered by 🚀
             </h1>
             <div className="py-2   text-xl  text-gray-700 list-disc dark:text-white">
-              <div className="flex justify-center items-center">
-                <SiReact />
-                &nbsp;React&nbsp;&nbsp;
-                <SiNextdotjs />
-                &nbsp;Next.js&nbsp;&nbsp;
-                <SiTailwindcss />
-                &nbsp;Tailwind&nbsp;&nbsp;
-                <GiRainbowStar />
-                &nbsp;my imagination
+              <div className="flex justify-center items-center flex-col gap-3 md:flex-row">
+                <div className="flex items-center">
+                  <SiReact />
+                  &nbsp;React
+                </div>
+                <div className="flex items-center">
+                  <SiNextdotjs />
+                  &nbsp;Next.js
+                </div>
+                <div className="flex items-center">
+                  <SiTailwindcss />
+                  &nbsp;Tailwind
+                </div>
+                <div className="flex items-center">
+                  <SiVercel />
+                  &nbsp;Vercel
+                </div>
+                <div className="flex items-center">
+                  <GiRainbowStar />
+                  &nbsp;my imagination
+                </div>
               </div>
             </div>
           </section>
