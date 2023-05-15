8fcfdbda606774f8b42870f05fd6e4620bc6de66
Thierry <tianming.chen0424@gmail.com>
2023-05-15 12:52:21 -0400

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8120375529346473"
+     crossorigin="anonymous"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
